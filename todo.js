1fe076be9ac86f31e69e441cd269031376a7f8d1
rayanahmed <thismac@Shrabonis-Air.Dlink>
2025-01-14 21:42:21 +0600

    the project is done

---
+++ todo.js
@@ -0,0 +1,71 @@
+const todoContainer = document.getElementById("todoContainer"); //get the ul
+const todoInput = document.getElementById("todoInput"); //get the input field
+const todoButton = document.getElementById("addTodo"); // working done
+
+todoButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  addTodo(e);
+});
+
+function addTodo() {
+  const todoText = todoInput.value.trim();
+  if (todoText === "") {
+    return;
+  }
+  const todoItem = document.createElement("li");
+
+  todoItem.innerHTML = `   
+  <div class="flex justify-between items-center gap-2 pt-4 border-gray-700">
+        <div class="flex items-center truncate w-full  gap-2 ">
+            <input type="checkbox" class="checkbox" />
+            <span id="todoText" class="text-wrap">${todoText}</span>
+        </div>
+        <div class="flex-shrink-0">
+            <button id="edit" class="btn btn-primary">Edit</button>
+            <button id="delete"  class="btn btn-error">Delete</button>
+        </div>
+ </div>`;
+  checkboxCheck(todoItem);
+  editTodo(todoItem);
+  deleteTodo(todoItem);
+  todoContainer.appendChild(todoItem);
+  todoInput.value = "";
+  const todos = Array.from(todoContainer.children);
+  todos.reverse();
+  todos.forEach((todo) => todoContainer.appendChild(todo));
+}
+
+function checkboxCheck(todoItem) {
+  const checkbox = todoItem.querySelector(".checkbox");
+  const inputText = todoItem.querySelector("#todoText");
+  checkbox.addEventListener("change", function () {
+    if (checkbox.checked) {
+      inputText.classList.add("line-through");
+      inputText.classList.add("text-red-500");
+    } else {
+      inputText.classList.remove("line-through");
+      inputText.classList.remove("text-red-500");
+    }
+  });
+}
+
+function editTodo(todoItem) {
+  const editButton = todoItem.querySelector("#edit");
+  const inputText = todoItem.querySelector("#todoText");
+  editButton.addEventListener("click", () => {
+    todoInput.value = inputText.textContent;
+    todoButton.textContent = "Update";
+    todoButton.classList.remove("btn-primary");
+    todoButton.classList.add("btn-success");
+    todoContainer.removeChild(todoItem);
+  });
+  todoButton.textContent = "Add";
+  todoButton.classList.remove("btn-success");
+}
+
+function deleteTodo(todoItem) {
+  const deleteButton = todoItem.querySelector("#delete");
+  deleteButton.addEventListener("click", () => {
+    todoContainer.removeChild(todoItem);
+  });
+}
